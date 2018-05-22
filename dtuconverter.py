a6255d2e90e5b75a053f603dde4606905fb246f3
lenaumova <38700578+lenaumova@users.noreply.github.com>
2018-05-22 14:45:24 +0300

    Add files via upload

---
+++ dtuconverter.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+import io
+import os
+import sys
+import numpy as np
+from matplotlib import pyplot as plt, rc
+import wfdb
+from dtureader import dturead
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) > 1:
+        fn = sys.argv[1]
+    else:
+        fn = "C:\PyFile\кроль_13_окт\s171012_141529\стабилизация.dtu"
+
+    d, fs1, n = dturead(fn)
+    signal = d
+    n_sig = 6
+    wfdb.wrsamp("ecgrecm", fs = 4000, units=["mV"]*n_sig,
+                sig_name=["ch1otv1", "ch1otv2", "ch1otv3", "ch2ovt1", "ch2otv2", "ch2otv3"],
+                p_signal = signal, fmt=["16"]*n_sig, write_dir="C:\PyFile\ecg")
+    
+    signals, fields = wfdb.rdsamp("C:\PyFile\ecg\ecgrecm", sampfrom=0, sampto="end", channels="all")
+    
+    fs = 4000
+    t = np.arange(0, len(signals)/fs, 1.0/fs)
+
+    plt.plot(t, signals)
+    plt.xlim([0, 0.01])
+    plt.title("Signals")
+
+    print("Look at the plots")
+    plt.show()
